c604bcd7c84dc404381538f0e64f89ffef80cb75
Alexr2391 <alexr2391@gmail.com>
2026-03-19 09:09:03 +0200

    feat(real-estate-app:FR1): create initial set up

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const config = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['import', '@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin: import/warnings',
+        'plugin: import/typescript',
+        'plugin: @typescript-eslint/recommended',
+        'prettier'
+    ],
+    rule: {
+        'import/no-unresolved': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argIgnorePattern: '^_', varsIgnorePattern: '^_' }
+        ]
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {}
+        }
+    }
+}
+
+export default config;
